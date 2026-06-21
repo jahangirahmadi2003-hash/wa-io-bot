b2f49a0bfbcc1691d41492d5435fb6d36c199eb9
jahangirahmadi2003-hash <jahangirahmadi2003@gmail.com>
2026-06-21 07:36:01 +0430

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = "8721962554:AAFChyqOAgqt93Kc3jeGjohOg1uuyip7XV8"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("⚡ Welcome to WA.io Support Bot")
